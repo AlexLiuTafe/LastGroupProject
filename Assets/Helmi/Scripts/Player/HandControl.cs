f2fc00be5bf67b1a281aa00599a1ba308fe66894
AlexLiuTafe <49624479+AlexLiuTafe@users.noreply.github.com>
2019-10-30 10:26:46 +1100

    Initial commit

---
+++ Assets/Helmi/Scripts/Player/HandControl.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandControl : MonoBehaviour
+{
+    public float moveSpeed = 2;
+    public Transform handHolder;
+
+
+    [Header("Field Of View")]
+    public float ironSightFOV = 10f; // Default
+    public float normalFOV = 50f; // Default
+
+    [Header("Position & Rotation")]
+    
+    public Vector3 ironSightPosition;
+    public Vector3 ironSightRotation;
+
+    [Header("References")]
+    public ParticleSystem[] muzzleFlash;
+    public AudioSource gunShotAudioSource;
+    public GameObject impactPrefab;
+    public Animator weaponAnimator;
+    public CameraShake camShake;
+
+    private PlayerInput _plyrInput;
+    private WaitForSeconds _fRate;
+
+    private float _targetShake;
+    private float _cameraFOVVelocity = 0;
+
+    private Camera _mainCam;
+    private CameraControl _camControl;
+    private Transform _camShake;
+
+    private Quaternion _originalRotation;
+    private Quaternion _targetRotation;
+    private Vector3 _originalPosition;
+    private Vector3 _targetPosition;
+    private Vector3 _camShakeInitialPos;
+    private Vector3 _cameraPosition;
+
+
+    private void Start()
+    {
+        _plyrInput = GetComponent<PlayerInput>();
+        _camControl = GetComponentInChildren<CameraControl>();
+        camShake = FindObjectOfType<CameraShake>();
+        _mainCam = Camera.main;
+        _camShake = camShake.transform;
+        _camShakeInitialPos = camShake.transform.localPosition;
+        _mainCam.fieldOfView = normalFOV;
+        _originalPosition = Vector3.zero;
+        _originalRotation = Quaternion.identity;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_plyrInput.scope)
+        {
+            handHolder.localPosition = Vector3.Lerp(handHolder.localPosition, ironSightPosition, Time.deltaTime * moveSpeed);
+            _targetShake = 0;
+            _mainCam.fieldOfView = Mathf.SmoothDamp(_mainCam.fieldOfView, ironSightFOV, ref _cameraFOVVelocity, moveSpeed * 6);
+            _camShake.localPosition = Vector3.Lerp(_camShake.localPosition, _cameraPosition, Time.deltaTime * moveSpeed);
+        }
+        else
+        {
+            handHolder.localPosition = Vector3.Lerp(handHolder.localPosition, _originalPosition, Time.deltaTime * moveSpeed);
+            _targetShake = 0.1f;
+            _mainCam.fieldOfView = Mathf.SmoothDamp(_mainCam.fieldOfView, normalFOV, ref _cameraFOVVelocity, moveSpeed * 6);
+            _camShake.localPosition = Vector3.Lerp(_camShake.localPosition, _camShakeInitialPos, Time.deltaTime * moveSpeed);
+           // _camShake.localPosition = _camShakeInitialPos;
+        }
+    }
+}
